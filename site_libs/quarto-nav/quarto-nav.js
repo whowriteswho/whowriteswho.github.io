2373fdf13c5fb3990df37fde3dafd2e38ea1231c
whowriteswho <drserdarbalci@gmail.com>
2022-04-06 17:56:42 +0000

    Update from https://github.com/whowriteswho/whowriteswho/commit/ed2a0e16761894f4f1d2b04eaca4a230b60f3246

--- site_libs/quarto-nav/quarto-nav.js
+++ site_libs/quarto-nav/quarto-nav.js
@@ -28,20 +28,7 @@ window.document.addEventListener("DOMContentLoaded", function () {
   function headerOffset() {
     // Set an offset if there is are fixed top navbar
     const headerEl = window.document.querySelector("header.fixed-top");
-    if (headerEl) {
-      return headerEl.clientHeight;
-    } else {
-      return 0;
-    }
-  }
-
-  function footerOffset() {
-    const footerEl = window.document.querySelector("footer.footer");
-    if (footerEl) {
-      return footerEl.clientHeight;
-    } else {
-      return 0;
-    }
+    return headerEl.clientHeight;
   }
 
   function updateDocumentOffsetWithoutAnimation() {
@@ -50,16 +37,13 @@ window.document.addEventListener("DOMContentLoaded", function () {
 
   function updateDocumentOffset(animated) {
     // set body offset
-    const topOffset = headerOffset();
-    const bodyOffset = topOffset + footerOffset();
+    const offset = headerOffset();
     const bodyEl = window.document.body;
-    bodyEl.setAttribute("data-bs-offset", topOffset);
-    bodyEl.style.paddingTop = topOffset + "px";
+    bodyEl.setAttribute("data-bs-offset", offset);
+    bodyEl.style.paddingTop = offset + "px";
 
     // deal with sidebar offsets
-    const sidebars = window.document.querySelectorAll(
-      ".sidebar, .headroom-target"
-    );
+    const sidebars = window.document.querySelectorAll(".sidebar");
     sidebars.forEach((sidebar) => {
       if (!animated) {
         sidebar.classList.add("notransition");
@@ -73,15 +57,15 @@ window.document.addEventListener("DOMContentLoaded", function () {
         sidebar.style.top = "0";
         sidebar.style.maxHeight = "100vh";
       } else {
-        sidebar.style.top = topOffset + "px";
-        sidebar.style.maxHeight = "calc(100vh - " + topOffset + "px)";
+        sidebar.style.top = offset + "px";
+        sidebar.style.maxHeight = "calc(100vh - " + offset + "px)";
       }
     });
 
     // allow space for footer
     const mainContainer = window.document.querySelector(".quarto-container");
     if (mainContainer) {
-      mainContainer.style.minHeight = "calc(100vh - " + bodyOffset + "px)";
+      mainContainer.style.minHeight = "calc(100vh - " + offset + "px)";
     }
 
     // link offset
@@ -93,14 +77,14 @@ window.document.addEventListener("DOMContentLoaded", function () {
     while (linkStyle.firstChild) {
       linkStyle.removeChild(linkStyle.firstChild);
     }
-    if (topOffset > 0) {
+    if (offset > 0) {
       linkStyle.appendChild(
         window.document.createTextNode(`
       section:target::before {
         content: "";
         display: block;
-        height: ${topOffset}px;
-        margin: -${topOffset}px 0 0;
+        height: ${offset}px;
+        margin: -${offset}px 0 0;
       }`)
       );
     }
@@ -116,18 +100,14 @@ window.document.addEventListener("DOMContentLoaded", function () {
     const headroom = new window.Headroom(header, {
       tolerance: 5,
       onPin: function () {
-        const sidebars = window.document.querySelectorAll(
-          ".sidebar, .headroom-target"
-        );
+        const sidebars = window.document.querySelectorAll(".sidebar");
         sidebars.forEach((sidebar) => {
           sidebar.classList.remove("sidebar-unpinned");
         });
         updateDocumentOffset();
       },
       onUnpin: function () {
-        const sidebars = window.document.querySelectorAll(
-          ".sidebar, .headroom-target"
-        );
+        const sidebars = window.document.querySelectorAll(".sidebar");
         sidebars.forEach((sidebar) => {
           sidebar.classList.add("sidebar-unpinned");
         });
@@ -150,7 +130,7 @@ window.document.addEventListener("DOMContentLoaded", function () {
 
   // Observe size changed for the header
   const headerEl = window.document.querySelector("header.fixed-top");
-  if (headerEl && window.ResizeObserver) {
+  if (window.ResizeObserver) {
     const observer = new window.ResizeObserver(
       throttle(updateDocumentOffsetWithoutAnimation, 50)
     );
